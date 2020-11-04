d45a223b32cee9b057808a3709ab4eb2ea1a3f9d
gxf <48995577+gxf1212@users.noreply.github.com>
2020-11-04 23:00:26 +0800

    upload gxf's trial

---
+++ try_gxf/main.py
@@ -0,0 +1,70 @@
+# deep learning project
+# sequence-based binary classification
+#TODO: push to GitHub
+
+""" requirements
+每个功能块必须加注释
+必须设定“__main__”函数,不能直接在空py文件上开始写！
+独立功能单元必须放到独立的包里，在主程序中通过“import”导入
+变量与命名规范
+对于类：类名首字母必须大写！构造函数与析构函数必须写清楚！类的方法采用全小写标志！类内私有变量一律以下划线开头！
+对于变量：一切变量采用驼峰命名法
+对于方法：方法本体在一行或者三行以内的一律使用匿名函数
+"""
+
+#%% import modules
+from model_lstm import *
+
+
+# %% read files
+# X_onehot will be (m, Tx, num_classes), Y will be 0 or 1
+X, ids = read_files('all-seq')
+Y, _ = read_files('all-ids-phe')
+Y = np.array(Y,dtype='float64')
+X_onehot = seq_to_one_hot(X)
+# TODO: This step cost much time! Need to improve!
+# solution: save preprocessed data
+np.savez('XY.npz', X_onehot, Y)
+
+#%% load processed data to save time
+data = np.load('XY.npz')
+X_onehot = data['arr_0']
+Y = data['arr_1']
+
+#%% divide sets
+X_train, Y_train, X_val, Y_val, X_test, Y_test = divide_train_test(X_onehot, Y, proportion=(0.8,0.1,0.1))
+
+#%% RNN model and training, not using now
+# the paper:
+# a (1-4-100-100-2048-2) architecture where 4-dimensional embeddings are passed through a
+# BLSTM, attention and fully connected layer
+# with 100, 100 and 2048 units respectively.
+# Values for batch size, dropout, recurrent dropout, and epochs
+# are tuned to 128, 0.5, 0.2 and 50 respectively.
+
+# model= RNN_model(Tx=X_train.shape[1], n_a=100, seq_classes = 2)
+
+
+#%% LSTM model
+# initialize. constants are imported from model_lstm
+m = X_train.shape[0]
+s0 = np.zeros((m, ns))
+
+encoder = model2_encoder(X_train.shape)
+decoder = model2_decoder((m, latent_units), Tx)
+model = encoder
+"""TODO:
+To train the autoencoder, we have to merge the two models; but to train the classifier, we need to
+merge the encoder and the simple RNN. It means that I had better write a class, three separate models
+and two merged models.
+Plus, I'm not sure how the weights are extracted and applied to another model.
+Moreover, a lot of hyper parameters are not determined:
+- the decoder (na, ns is just ok..)
+- the classifier and model 2a
+and integrated gradient paper?
+"""
+
+opt = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, decay=0.01)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit([X_train, s0], Y_train, epochs=100, batch_size=100)
+
---
+++ try_gxf/model_lstm.py
@@ -0,0 +1,176 @@
+from utils import *
+import tensorflow as tf
+from keras.models import Model, Sequential
+from keras.layers import Input, LSTM, Bidirectional, Dense, Activation
+from keras.layers import RepeatVector, Dot, Concatenate, SimpleRNN
+from keras.layers import Reshape, Lambda, Dropout
+# from keras.initializers import glorot_uniform
+from keras.optimizers import Adam
+from keras import backend as K
+from attention import Attention
+
+# define constants, most are hyper parameters
+latent_units = 256  # literature optimal value
+Tx = 1440  # length of input
+na = 32  # units in LSTM
+ns = 64  # units in post attention RNN
+
+
+# %% model2: LSTM
+# copied from deep learning course
+def softmax(x, axis=1):
+    """Softmax activation function.
+    # Arguments
+        x : Tensor.
+        axis: Integer, axis along which the softmax normalization is applied.
+    # Returns
+        Tensor, output of softmax transformation.
+    # Raises
+        ValueError: In case `dim(x) == 1`.
+    """
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
+
+
+# Define shared layers as **global variables**
+repeator = RepeatVector(Tx, name='repeat hidden')
+concatenator = Concatenate(axis=-1)
+# the two densors are all used to train for alpha, not in the main path
+densor1 = Dense(10, activation="tanh", name='dense1')
+densor2 = Dense(1, activation="relu", name='dense2')
+activator = Activation(softmax,
+                       name='attention_weights')  # We are using a custom softmax(axis = 1) loaded in this notebook
+dotor = Dot(axes=1)
+# after attention
+after_attention_layer = SimpleRNN(ns, return_state=True)  #TODO: na is another hyper parameter...
+out_encoder_layer = Dense(latent_units, activation=softmax, name='output encoder')
+
+
+def one_step_attention_encoder(a, s_prev):
+    """
+    Performs one step of attention: Outputs a context vector computed as a dot product of the attention weights
+    "alphas" and the hidden states "a" of the Bi-LSTM.
+
+    Arguments:
+    a -- hidden state output of the Bi-LSTM, numpy-array of shape (m, Tx, 2*n_a)
+    s_prev -- previous hidden state of the (post-attention) LSTM, numpy-array of shape (m, n_s)
+
+    Returns:
+    context -- context vector, input of the next (post-attetion) LSTM cell
+    """
+
+    # Use repeator to repeat s_prev (Tx times) to be of shape (m, Tx, n_s) so that you can concatenate it
+    # with all hidden states "a"
+    s_prev = repeator(s_prev)
+    # Use concatenator to concatenate a and s_prev on the last axis (≈ 1 line)
+    concat = concatenator([a, s_prev])  # (m, Tx, 2*na+ns)
+    # Use densor1 to propagate concat through a small fully-connected neural network to compute the "intermediate
+    # energies" variable e.
+    e = densor1(concat)  # (m, Tx, 10)
+    # Use densor2 to propagate e through a small fully-connected neural network
+    # to compute the "energies" variable energies.
+    energies = densor2(e)  # (m, Tx, 1)
+    # Use "activator" on "energies" to compute the attention weights "alphas" (≈ 1 line)
+    alphas = activator(energies)
+    # Use dotor together with "alphas" and "a" to compute the context vector to be given
+    # to the next (post-attention) LSTM-cell
+    context = dotor([alphas, a])
+
+    return context
+
+
+def model2_encoder(input_shape, latent_units=256):
+    # preparations
+    m, Tx, input_cls = input_shape  # Tx=1440, cls=5
+    X = Input(shape=(Tx, input_cls), name='input')  # we don't assign the number of samples in the input
+    s0 = Input(shape=(ns,), name='post attention hidden')  # custom initial value (I don't know why...)
+    post_attn_hidden = s0  # # the hidden state of the post attention layer
+    output = []  # prepare a list for 256 outputs
+
+    # model
+    # pre-attention BiLSTM, no return seq; we just input the dimension but no output length
+    encoder_lstm = Bidirectional(LSTM(na, return_sequences=False))(X)  # (m, Tx, input_cls) -> (m, Tx, 2*na)
+    for i in range(latent_units):  # this range determines the output dimension
+        # we share layers in the attention part
+        # output all calculated attention vectors, which have been linear combined
+        context = one_step_attention_encoder(encoder_lstm, post_attn_hidden)  # (m, Tx, 2*na) -> (m, 1, 2*na)
+        # input the attention vectors to a simple RNN, and return sequence
+        post_attn_hidden = after_attention_layer(context, inputs=post_attn_hidden)  # (m, 1, 2*na) -> (m, 1, ns)
+        # input the hidden state into dense layer to get an element of the latent representation
+        out = out_encoder_layer(post_attn_hidden)  # (m, 1, ns) -> (m, 1, 1)
+        output.append(out)
+    model = Model(inputs=[X, s0], outputs=np.array(output, shape=(m, latent_units)))
+    return model
+
+
+# Define shared layers again for the decoder
+repeator2 = RepeatVector(latent_units, name='repeat hidden')
+concatenator2 = Concatenate(axis=-1)
+# the two densors are all used to train for alpha, not in the main path
+densor1_2 = Dense(10, activation="tanh", name='dense1')
+densor2_2 = Dense(1, activation="relu", name='dense2')
+activator2 = Activation(softmax,
+                       name='attention_weights')  # We are using a custom softmax(axis = 1) loaded in this notebook
+dotor2 = Dot(axes=1)
+# after attention
+after_attention_layer2 = SimpleRNN(ns, return_state=True)
+out_encoder_layer2 = Dense(Tx, activation=softmax, name='output encoder')
+
+
+def one_step_attention_decoder(a, s_prev):
+    s_prev = repeator2(s_prev)
+    concat = concatenator2([a, s_prev])
+    e = densor1_2(concat)
+    energies = densor2_2(e)
+    alphas = activator2(energies)
+    context = dotor2([alphas, a])
+
+    return context
+
+
+# decoder can have a different structure from encoder as long as it can regenerate the input sequences
+def model2_decoder(input_shape, Tx):
+    # it's not a good solution because I did not refer to anything but repeated LSTM and attention
+    m, latent_units = input_shape  # latent = 256, Tx = 1440
+    X = Input(shape=(latent_units))  # input shape=(m, latent_units)
+    s0 = Input(shape=(ns,), name='post attention hidden')
+    post_attn_hidden = s0
+    output = []
+
+    # model
+    # na, ns value should change? since the input has only one value...
+    decoder_lstm = LSTM(na, return_sequences=False)(X)
+    for i in range(Tx):
+        context = one_step_attention_decoder(decoder_lstm, post_attn_hidden)
+        post_attn_hidden = after_attention_layer2(context, inputs=post_attn_hidden)
+        out = out_encoder_layer2(post_attn_hidden)
+        output.append(out)
+    model = Model(inputs=[X, s0], outputs=np.array(output, shape=(m, Tx)))
+    return model
+
+
+# %% another version: sequential
+# def model2(input_shape):
+#     m, Tx, input_cls = input_shape
+#     model = Sequential()
+#     model.add(Bidirectional(LSTM(lstm_units, return_sequences=False)))  # not assigning activation
+#     return model
+
+'''
+note:
+1. `return_state` in LSTM means assigning x^{t} with y^{t-1}. In other words, the output at a time is going to 
+be the input at the next time step.
+2. `Dot` layer (two inputted tensors) seems to multiply the assigned dimension without changing others.
+I tried to describe the rules:
+- if we only assign one dimension, the two same dimension will be eliminated (1 by n dot n by 1)
+- the two elements in the axes tuple (the outest, maybe list) refer to two tensors; len(list) could be > 2
+refer to numpy.tensordot: https://numpy.org/doc/stable/reference/generated/numpy.tensordot.html#numpy.tensordot
+and: https://blog.csdn.net/wdh315172/article/details/105973194?utm_medium=distribute.pc_relevant.none-task-blog-title-7&spm=1001.2101.3001.4242
+'''
