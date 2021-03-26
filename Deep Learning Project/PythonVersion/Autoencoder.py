5d7b2fa263f56b9b680567bcdde7d81fbc015fb4
michaelGuo1204 <2236536804@qq.com>
2021-03-26 21:32:36 +0800

    Update encoded latent

--- Deep Learning Project/PythonVersion/Autoencoder.py
+++ Deep Learning Project/PythonVersion/Autoencoder.py
@@ -1,16 +1,30 @@
-from random import randint
-from numpy import load
-from numpy import array
-from numpy import argmax
-from numpy import array_equal
-from Model import Autoencoder_Model,divide_train_test
+import tensorflow as tf
+from Model import *
 
-#%% Load Data
-data = load('XY.npz')
-X_onehot = data['arr_0']
-Y = data['arr_1']
-X_train, Y_train, X_val, Y_val, X_test, Y_test = divide_train_test(X_onehot, Y, proportion=(0.8,0.1,0.1))
 
-#%% Set Model
-autoencoder=Autoencoder_Model(ifAttention=True)
-autoencoder.trainModel(X_train,Y_train,X_val,Y_val)
+
+if __name__=='__main__':
+    physical_devices = tf.config.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+
+    if(os.path.exists('./saved_model/base_model') or os.path.exists('./saved_model/attention_model')):
+        autoencoder=models.load_model('./saved_model/base_model')
+        autoencoder.summary()
+        autoencoder=Autoencoder_Model(autoencoder)
+
+    else:
+        #%% Load Data
+        data = load('XY.npz')
+        X_onehot = data['arr_0']
+        Y = data['arr_1']
+        X_train, Y_train, X_val, Y_val, X_test, Y_test = divide_train_test(X_onehot, Y, proportion=(0.8,0.1,0.1))
+
+        #%% Set Model
+        autoencoder=Autoencoder_Model(ifAttention=False)
+        autoencoder.trainModel(X_train,Y_train,X_val,Y_val)
+
+    data = load('XY.npz')
+    X_onehot = data['arr_0']
+    Y = data['arr_1']
+    autoencoder.encoderPredict(X_onehot,Y)
+
--- Deep Learning Project/PythonVersion/Model.py
+++ Deep Learning Project/PythonVersion/Model.py
@@ -1,13 +1,14 @@
-from tensorflow.keras import Input,Model
-from tensorflow.keras.layers import LSTM
+from tensorflow.keras import Input,Model,models
+from tensorflow.keras.layers import LSTM,Bidirectional
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import TimeDistributed
 from tensorflow.keras.layers import RepeatVector
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint,TensorBoard
 from tensorflow.keras.utils import plot_model
 from attention import Attention
-from numpy import random
+from numpy import random,load,array,append,savez
 import os.path
+import datetime
 
 def divide_train_test(X_all, Y_all, proportion=(0.7,0.2,0.1)):
     train, val, test = proportion
@@ -34,13 +35,19 @@ class Autoencoder_Model:
         self._features=5 #There are five features in DNA's one-hot model
         self._sequence_long=1444 #The adjusted sequence long is 1444 bps
         self._lstm_neurons=256 #Set lstm neurons are 256
-        self._train_epoch=5000 #Set train epoch 5000
+        self._train_epoch=100 #Set train epoch 100
         self._batch_size=1
         if ifAttention:
             self.isAttention=True
             self._autoencoder_model,self._encoder_model = self.attentionModel()
         else:
             self._autoencoder_model,self._encoder_model = self.baseModel()
+
+    def __init__(self,autoencoder):
+        self._autoencoder_model=autoencoder
+        self._encoder_model=Model(self._autoencoder_model.input,
+                                  self._autoencoder_model.layers[-3].output)
+        self._encoder_model.summary()
 
     def attentionModel(self):
         inputs = Input(shape=(self._sequence_long, self._features))
@@ -50,23 +57,25 @@ class Autoencoder_Model:
                        )(inputs)
         decoded = Attention()(encoded)
         decoded = RepeatVector(self._sequence_long)(decoded)
-        decoded = LSTM(self._features, return_sequences=True)(decoded)
+        decoded = LSTM(self._features)(decoded)
         autoencoder = Model(inputs=inputs, outputs=decoded)
         encoder = Model(inputs=inputs, outputs=encoded)
         autoencoder.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         autoencoder.summary()
-        plot_model(autoencoder,dpi=328,show_shapes=True,show_dtype=True)
+        #plot_model(autoencoder,dpi=328,show_shapes=True,show_dtype=True)
         return autoencoder, encoder
+
     def baseModel(self):
         inputs = Input(shape=(self._sequence_long,self._features))
         encoded = LSTM(self._lstm_neurons)(inputs)
         decoded = RepeatVector(self._sequence_long)(encoded)
-        decoded = LSTM(self._features, return_sequences=True)(decoded)
+        decoded = LSTM(self._features,return_sequences=True)(decoded)
         autoencoder = Model(inputs=inputs, outputs=decoded)
         encoder = Model(inputs=inputs, outputs=encoded)
         autoencoder.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         autoencoder.summary()
         return autoencoder,encoder
+
     def trainModel(self,X_train,Y_train,X_valid,Y_valid):
         checkpoint_path = "./checkpoint/Attention_checkpoint.ckpt"
         checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -74,6 +83,16 @@ class Autoencoder_Model:
         cp_callback = ModelCheckpoint(filepath=checkpoint_dir,
                                                          save_weights_only=True,
                                                          verbose=1)
+        log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        tb_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
         print("=========Begin=======")
-        self._autoencoder_model.fit(X_train,X_train,epochs=self._train_epoch,verbose=1,validation_data=(X_valid,X_valid),callbacks=[cp_callback])
-        self._autoencoder_model.save('saved_model/attention_model')
+        self._autoencoder_model.fit(X_train,X_train,epochs=self._train_epoch,verbose=1,validation_data=(X_valid,X_valid),callbacks=[cp_callback,tb_callback])
+        self._autoencoder_model.save('saved_model/base_model')
+
+    def encoderPredict(self,X,y):
+        predicts=[]
+        for sample in X:
+            predicted=self._encoder_model.predict(sample.reshape((1,sample.shape[0],sample.shape[1])),verbose=1)
+            predicts.append(predicted)
+        savez('./latent.npz',array(predicts,dtype=float),array(y,dtype=float))
+
